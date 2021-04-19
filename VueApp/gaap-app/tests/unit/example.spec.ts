f347f893e859b779d88ea4a9264389602902319c
Emilio Rivas <miles.fra.2000@gmail.com>
2021-04-19 00:25:39 -0500

    Test de modulo agregados

--- VueApp/gaap-app/tests/unit/example.spec.ts
+++ VueApp/gaap-app/tests/unit/example.spec.ts
@@ -1,12 +1,54 @@
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
+import GaapInfo from "@/components/GaapInfo.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "new message";
+    const msg = "Header Text";
     const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
+      props: { msg},
     });
     expect(wrapper.text()).toMatch(msg);
+  });
+});
+
+describe("HelloWorld.vue", () => {
+  it("renders props.msg when passed", () => {
+    const cont = "Body Content";
+    const wrapper = shallowMount(HelloWorld, {
+      props: { cont },
+    });
+    expect(wrapper.text()).toMatch(cont);
+  });
+});
+
+describe("GaapInfo.vue", () => {
+  it("renders props.msg when passed", () => {
+    const mision = "Text content";
+    const wrapper = shallowMount(GaapInfo, {
+      props: { mision },
+    });
+    expect(wrapper.text()).toMatch(mision);
+  });
+});
+
+describe("GaapInfo.vue", () => {
+  it("renders props.msg when passed", () => {
+    const instalaciones = "Text content";
+    const wrapper = shallowMount(GaapInfo, {
+      props: { instalaciones },
+    });
+    expect(wrapper.text()).toMatch(instalaciones);
+  });
+});
+
+describe("GaapInfo.vue", () => {
+  it("renders props.msg when passed", () => {
+    const equipo = "Text content";
+    const wrapper = shallowMount(GaapInfo, {
+      props: { equipo },
+    });
+    expect(wrapper.text()).toMatch(equipo);
   });
 });
