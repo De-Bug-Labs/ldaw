5f5cd58eaee808e0744703bb5cf449d2a0ffd9ce
jimmedto <A01700190@itesm.mx>
2021-04-26 20:17:50 -0500

    Opcion 2 para conexion

--- src/views/About.vue
+++ src/views/About.vue
@@ -19,31 +19,23 @@ export default defineComponent({
   },
 
   methods: {
-    /*  getInfo(){
-      try{
-        const data = fetch('http://localhost:5000/api/information/03ea46a0-6ddd-45dd-9c56-ab7f53f6d0a3')
-        .then((res) => res.json())
-        .then((data) => {
-          this.information = data;
-        });
+    getInfo() {
+      try {
+        const data = fetch(
+          "http://localhost:5000/api/information/03ea46a0-6ddd-45dd-9c56-ab7f53f6d0a3"
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            this.information = data;
+          });
       } catch (error) {
         console.log(error);
       }
-    }, */
+    },
   },
 
   mounted() {
-    try {
-      const data = fetch(
-        "http://localhost:5000/api/information/03ea46a0-6ddd-45dd-9c56-ab7f53f6d0a3"
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          this.information = data;
-        });
-    } catch (error) {
-      console.log(error);
-    }
+    this.getInfo();
   },
 });
 </script>
