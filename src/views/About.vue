c0485a09db528e8365b6b20ac9b25add57739e67
jimmedto <A01700190@itesm.mx>
2021-05-09 16:04:38 -0500

    Cambiar consultar info

--- src/views/About.vue
+++ src/views/About.vue
@@ -22,7 +22,7 @@ export default defineComponent({
   methods: {
     getInfo() {
       try {
-        const data = fetch("http://localhost:5000/api/information/1")
+        const data = fetch("http://localhost:5000/api/information")
           .then((res) => res.json())
           .then((data) => {
             this.information = data;
