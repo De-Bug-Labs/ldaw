aead2c3b8a149fd8c87cbe5fcf5d1c8585128b51
Bernardo Estrada <estradaf.bernardo@gmail.com>
2021-05-24 18:58:55 -0500

    Merge conflicts

--- src/views/Material.vue
+++ src/views/Material.vue
@@ -60,9 +60,7 @@ export default defineComponent({
   methods: {
     getInfo() {
       try {
-        const data = fetch(
-          this.apiUrl + "material?page=" + this.page + "&pageSize=8"
-        )
+        const data = fetch("/api/material?page=" + this.page + "&pageSize=8")
           .then((res) => res.json())
           .then((data) => {
             this.materiales = data;
@@ -74,7 +72,7 @@ export default defineComponent({
     },
     getPages() {
       try {
-        const data = fetch(this.apiUrl + "material/pages?page=0&pageSize=8")
+        const data = fetch("/api/material/pages?page=0&pageSize=8")
           .then((res) => res.json())
           .then((data) => {
             this.pages = data;
