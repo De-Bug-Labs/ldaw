7a6b5a29f010d44f657a69f103db4930185f4c7e
Emilio Rivas <miles.fra.2000@gmail.com>
2021-06-02 18:43:49 -0500

    logo fix electric boogaloo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="nav" v-if="regular">
     <router-link to="/" class="logo1"
-      ><img src="../public/img/roof.svg" />
-      <p>GAAP</p></router-link
-    >
+      ><img src="../public/img/logo1.png" />
+    </router-link>
     <a class="toggle" @click="mobile = !mobile"
       ><i class="large material-icons">dehaze</i></a
     >
@@ -119,7 +118,7 @@ body {
     display: flex;
     flex-direction: row;
     text-align: center;
-    height: 70px;
+    height: 80px;
     transition: 0.5s;
 
     p {
@@ -133,7 +132,8 @@ body {
     }
 
     img {
-      height: 70px;
+      height: 80px;
+      width: 200px;
       transition: 0.5s;
     }
   }
