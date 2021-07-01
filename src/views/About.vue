60074704ac08c00ed97dfa6466cd281b8ed97515
Alberto Matute <69598304+AlbertoMatuteB@users.noreply.github.com>
2021-06-30 21:15:50 -0500

    fix puntuacion

--- src/views/About.vue
+++ src/views/About.vue
@@ -16,11 +16,11 @@ export default defineComponent({
     return {
       information: {
         team:
-          "Contamos con un equipo de PROFESIONISTAS integrado por Médicos Generales, Médico en Rehabilitación, Terapeuta Físico, Odontólogo, Nutrióloga, Tanatóloga.Contadores Públicos, Licenciada en Artes Escénicas, Licenciada en Comunicación.Jóvenes de Servicio Social y Voluntarios.",
+          "Contamos con un equipo de PROFESIONISTAS integrado por Médicos Generales, Médico en Rehabilitación, Terapeuta Físico, Odontólogo, Nutrióloga, Tanatóloga, Contadores Públicos, Licenciada en Artes Escénicas, Licenciada en Comunicación, Jóvenes de Servicio Social y Voluntarios.",
         instalation:
-          "Contamos con áreas de atención  para personas de la tercera edad a través del servicio de Medicina General, Rehabilitación y Terapia Física, Atención Dental, Nutrición y Tanatología.De igual manera se cuenta con Talleres de Baile, Tejido, Taller Manejo de celulares, Terapia de la Memoria.",
+          "Contamos con áreas de atención para personas de la tercera edad a través del servicio de Medicina General, Rehabilitación, Terapia Física, Atención Dental, Nutrición y Tanatología. De igual manera se cuenta con Talleres de Baile, Tejido, Taller Manejo de celulares, Terapia de la Memoria.",
         mision:
-          "Brindar a nuestros beneficiarios adultos y adultos mayores un servicio integral humano, personalizado y de calidad, con una metodología activa, buscando la incorporación y  participación del adulto mayor con sus familiares , fortaleciendo su integración y el reconocimiento de lo invaluable de su persona.",
+          "Brindar a nuestros beneficiarios adultos y adultos mayores un servicio integral humano, personalizado y de calidad, con una metodología activa, buscando la incorporación y participación del adulto mayor con sus familiares, fortaleciendo su integración y el reconocimiento de lo invaluable de su persona.",
       },
       completed: false,
     };
